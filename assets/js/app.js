09bb0409000efe8f8e2fcdd2e102e560648d89ba
Dadis <raditztv@gmail.com>
2025-02-14 00:06:46 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, render.domElement);
