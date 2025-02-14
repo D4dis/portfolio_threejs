56d931bb81960c57be1b8336dd4930bd8e61df7d
Dadis <raditztv@gmail.com>
2025-02-15 00:35:27 +0100

    threejs working, rotating cube

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,29 @@
 import * as THREE from 'three';
 
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+const canvas = document.getElementById('canvas');
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-const controls = new OrbitControls(camera, render.domElement);
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+camera.position.set(0, 0, 2);
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+loop();
+
+function loop(){
+  requestAnimationFrame(loop);
+  mesh.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
