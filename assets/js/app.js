1360217952181cbb548930898d8a8aebd8885087
Dadis <raditztv@gmail.com>
2025-02-15 13:31:59 +0100

    green cube

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 const mesh = new THREE.Mesh(geometry, material);
 
