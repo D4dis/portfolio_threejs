18b0aa831608c130bc634c37d2deca03ec1d5b22
Dadis <raditztv@gmail.com>
2025-02-25 01:19:22 +0100

    start of tank

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 
-import GUI from 'lil-gui';
+// import GUI from 'lil-gui';
 
-const gui = new GUI();
+// const gui = new GUI();
 
 const canvas = document.getElementById('canvas');
 
@@ -107,135 +107,143 @@ const canvas = document.getElementById('canvas');
 
 //SUN MOON
 
+// const scene = new THREE.Scene();
+
+// const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+// camera.position.set(0, 50, 0);
+// camera.up.set(0, 0, 1);
+// camera.lookAt(0, 0, 0);
+
+// const renderer = new THREE.WebGLRenderer({ canvas });
+
+// const objects = [];
+
+// const radius = 1;
+// const widthSegments = 6;
+// const heightSegments = 6;
+// const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+
+// const solarSystem = new THREE.Object3D();
+// scene.add(solarSystem);
+// objects.push(solarSystem);
+
+// const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
+// const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+// sunMesh.scale.set(5, 5, 5);
+// solarSystem.add(sunMesh);
+// objects.push(sunMesh);
+
+// const earthOrbit = new THREE.Object3D();
+// earthOrbit.position.x = 10;
+// solarSystem.add(earthOrbit);
+// objects.push(earthOrbit);
+
+// const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x2233FF, emissive: 0x112244 });
+// const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+// earthOrbit.add(earthMesh);
+// objects.push(earthMesh);
+
+// const moonOrbit = new THREE.Object3D();
+// moonOrbit.position.x = 2;
+// earthOrbit.add(moonOrbit);
+
+// const moonMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, emissive: 0x222222 });
+// const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+// moonMesh.scale.set(.5, .5, .5);
+// moonOrbit.add(moonMesh);
+// objects.push(moonMesh);
+
+// const light = new THREE.PointLight(0xFFFFFF, 500);
+// scene.add(light);
+
+
+// class AxisGridHelper {
+//   constructor(node, units = 10) {
+//     const axes = new THREE.AxesHelper();
+//     axes.material.depthTest = false;
+//     axes.renderOrder = 2;  // after the grid
+//     node.add(axes);
+
+//     const grid = new THREE.GridHelper(units, units);
+//     grid.material.depthTest = false;
+//     grid.renderOrder = 1;
+//     node.add(grid);
+
+//     this.grid = grid;
+//     this.axes = axes;
+//     this.visible = false;
+//   }
+//   get visible() {
+//     return this._visible;
+//   }
+//   set visible(v) {
+//     this._visible = v;
+//     this.grid.visible = v;
+//     this.axes.visible = v;
+//   }
+// }
+
+
+// function makeAxisGrid(node, label, units) {
+//   const helper = new AxisGridHelper(node, units);
+//   gui.add(helper, 'visible').name(label);
+// }
+
+// makeAxisGrid(solarSystem, 'solarSystem', 25);
+// makeAxisGrid(sunMesh, 'sunMesh');
+// makeAxisGrid(earthOrbit, 'earthOrbit');
+// makeAxisGrid(earthMesh, 'earthMesh');
+// makeAxisGrid(moonOrbit, 'moonOrbit');
+// makeAxisGrid(moonMesh, 'moonMesh');
+
+// function resizeRendererToDisplaySize(renderer) {
+
+//   const canvas = renderer.domElement;
+//   const width = canvas.clientWidth;
+//   const height = canvas.clientHeight;
+//   const needResize = canvas.width !== width || canvas.height !== height;
+//   if (needResize) {
+
+//     renderer.setSize(width, height, false);
+
+//   }
+
+//   return needResize;
+
+// }
+
+// function render(time) {
+
+//   time *= 0.001;
+
+//   if (resizeRendererToDisplaySize(renderer)) {
+
+//     const canvas = renderer.domElement;
+//     camera.aspect = canvas.clientWidth / canvas.clientHeight;
+//     camera.updateProjectionMatrix();
+
+//   }
+
+//   objects.forEach((obj) => {
+
+//     obj.rotation.y = time;
+
+//   });
+
+//   renderer.render(scene, camera);
+
+//   requestAnimationFrame(render);
+
+// }
+
+// requestAnimationFrame(render);
+
+// render(1);
+
+
+// TANK
+
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 50, 0);
-camera.up.set(0, 0, 1);
-camera.lookAt(0, 0, 0);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
 
-const renderer = new THREE.WebGLRenderer({ canvas });
-
-const objects = [];
-
-const radius = 1;
-const widthSegments = 6;
-const heightSegments = 6;
-const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
-
-const solarSystem = new THREE.Object3D();
-scene.add(solarSystem);
-objects.push(solarSystem);
-
-const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
-const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-sunMesh.scale.set(5, 5, 5);
-solarSystem.add(sunMesh);
-objects.push(sunMesh);
-
-const earthOrbit = new THREE.Object3D();
-earthOrbit.position.x = 10;
-solarSystem.add(earthOrbit);
-objects.push(earthOrbit);
-
-const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x2233FF, emissive: 0x112244 });
-const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
-earthOrbit.add(earthMesh);
-objects.push(earthMesh);
-
-const moonOrbit = new THREE.Object3D();
-moonOrbit.position.x = 2;
-earthOrbit.add(moonOrbit);
-
-const moonMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, emissive: 0x222222 });
-const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-moonMesh.scale.set(.5, .5, .5);
-moonOrbit.add(moonMesh);
-objects.push(moonMesh);
-
-const light = new THREE.PointLight(0xFFFFFF, 500);
-scene.add(light);
-
-
-class AxisGridHelper {
-  constructor(node, units = 10) {
-    const axes = new THREE.AxesHelper();
-    axes.material.depthTest = false;
-    axes.renderOrder = 2;  // after the grid
-    node.add(axes);
-
-    const grid = new THREE.GridHelper(units, units);
-    grid.material.depthTest = false;
-    grid.renderOrder = 1;
-    node.add(grid);
-
-    this.grid = grid;
-    this.axes = axes;
-    this.visible = false;
-  }
-  get visible() {
-    return this._visible;
-  }
-  set visible(v) {
-    this._visible = v;
-    this.grid.visible = v;
-    this.axes.visible = v;
-  }
-}
-
-
-function makeAxisGrid(node, label, units) {
-  const helper = new AxisGridHelper(node, units);
-  gui.add(helper, 'visible').name(label);
-}
-
-makeAxisGrid(solarSystem, 'solarSystem', 25);
-makeAxisGrid(sunMesh, 'sunMesh');
-makeAxisGrid(earthOrbit, 'earthOrbit');
-makeAxisGrid(earthMesh, 'earthMesh');
-makeAxisGrid(moonOrbit, 'moonOrbit');
-makeAxisGrid(moonMesh, 'moonMesh');
-
-function resizeRendererToDisplaySize(renderer) {
-
-  const canvas = renderer.domElement;
-  const width = canvas.clientWidth;
-  const height = canvas.clientHeight;
-  const needResize = canvas.width !== width || canvas.height !== height;
-  if (needResize) {
-
-    renderer.setSize(width, height, false);
-
-  }
-
-  return needResize;
-
-}
-
-function render(time) {
-
-  time *= 0.001;
-
-  if (resizeRendererToDisplaySize(renderer)) {
-
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
-
-  }
-
-  objects.forEach((obj) => {
-
-    obj.rotation.y = time;
-
-  });
-
-  renderer.render(scene, camera);
-
-  requestAnimationFrame(render);
-
-}
-
-requestAnimationFrame(render);
-
-render(1);
